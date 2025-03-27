aed943ddca8d4e107591c43ec62389033d1cba17
Luizmachado87 <luizmachado87@outlook.com>
2025-03-27 19:50:05 -0300

    ended commit

--- api_angular/src/app/app.component.ts
+++ api_angular/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,15 +10,33 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
-  moedas: any = [];
+  private apiUrl = 'https://api.api-ninjas.com/v1/animals';
+  private apiKey = 'or1IQXa6nfAUfxlLHha2+Q==hq9Q7EeQSrNMsoBe';
+
+  animal: any;
 
   constructor(private http: HttpClient){}
 
- /*  getMoeda(){
-    return this.http.get('https://')
-  } */
+  ngOnInit() {
+    this.getAnimal('cheetah').subscribe(
+      (data) => {
+        if (data && data.length > 0) {
+          this.animal = data[0]; // Assume que a API retorna um array
+        }
+      },
+      (error) => console.error('Erro:', error)
+    );
+  }
 
+
+  getAnimal(name: string): Observable<any> {
+    const headers = new HttpHeaders({
+      'X-Api-Key': this.apiKey
+    });
+
+    return this.http.get(`${this.apiUrl}?name=${name}`, { headers });
+  }
 }
 
